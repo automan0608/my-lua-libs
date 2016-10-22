12cf916f970aea23ed3f5d814bdc2d9dde371394
automan0608 <maszszs@sina.com>
2016-10-22 12:37:12 +0800

    add test directory

---
+++ src/test/01-libmath.lua
@@ -0,0 +1,13 @@
+
+
+local mymath = require "libmath"
+
+print('max: ' .. mymath.max(3,1,5,15,8,11,6))
+
+print('top: ' .. mymath.top())
+
+print('min: ' .. mymath.min(3,1,5,15,8,11,6))
+
+print('abs: ' .. mymath.abs(-200))
+
+print('top: ' .. mymath.top())
